f6c2715a28bf35f388f347f5725098bccc715431
CapLOBO <reshetnevdaniil@yandex.ru>
2023-10-24 13:16:59 +0300

    Небольшой фикс модели

--- incidents/models.py
+++ incidents/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Incident(models.Model):
     longitude = models.FloatField()
-    latitude = models.FloatField
+    latitude = models.FloatField()
     address = models.CharField(max_length=254)
     category = models.ForeignKey("Category", on_delete=models.PROTECT, related_name="incidents")
     is_predictive = models.BooleanField(default=False)
