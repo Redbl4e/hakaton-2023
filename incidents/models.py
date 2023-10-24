eba2d536e316d4bcfc211ab7d2f31ee2e409e73e
CapLOBO <reshetnevdaniil@yandex.ru>
2023-10-24 13:12:19 +0300

    Добавление модели инцидента

--- incidents/models.py
+++ incidents/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Incident(models.Model):
+    longitude = models.FloatField()
+    latitude = models.FloatField
+    address = models.CharField(max_length=254)
+    category = models.ForeignKey("Category", on_delete=models.PROTECT, related_name="incidents")
+    is_predictive = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def full_incident_name(self):
+        return f"the incident {self.category} at the address: {self.address} is registered"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
